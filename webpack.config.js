e15d11bdaf6f65f08d2e17429ab65a819c1ecccf
David <david@semke.ca>
2024-06-13 23:38:42 -0700

    Config: webpack extractComments: false

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path")
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: "./public/javascripts/root.js",
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "public", "javascripts"),
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
   },
   mode: "production",
 }
